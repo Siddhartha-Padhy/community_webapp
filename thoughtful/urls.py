3dcb5b3c1b3142c2dc236bf17b7a90dd739bd930
Siddhartha-Padhy <siddhartha6222@gmail.com>
2022-03-27 12:36:06 +0530

    feat: search results without refresh

--- thoughtful/urls.py
+++ thoughtful/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('home/', views.home, name='home'),
     path('explore/', views.explore, name='explore'),
+    path('search/<str:id>/', views.search_results, name='searchresults'),
     path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about'),
 ]
--- thoughtful/views.py
+++ thoughtful/views.py
@@ -1,5 +1,7 @@
+from time import sleep
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 def login(request):
     return render(request,'login.html')
@@ -38,6 +40,25 @@ def explore(request):
         # return render(request, 'explore.html', data)
     
     return render(request, 'explore.html', data)
+
+def search_results(request,id):
+    data = {
+        'results': [
+        {
+            'username': 'james_18',
+            'name': 'James Clear'
+        },
+        {
+            'username': 'nick_davidson',
+            'name': 'Nick Davidson'
+        },
+        {
+            'username': 'john_parkinson',
+            'name': 'John Parkinson'
+        }
+    ]
+    }
+    return JsonResponse(data)
 
 def profile(request):
     data = {
